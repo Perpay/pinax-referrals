f6901b09c1efe5cb5a35571c68d833e2ae172ca5
Aaron Ireland <aarireland@me.com>
2017-03-02 15:54:54 -0500

    Allow slugified referral codes

--- pinax/referrals/urls.py
+++ pinax/referrals/urls.py
@@ -5,5 +5,5 @@ from .views import create_referral, process_referral
 
 urlpatterns = [
     url(r"^$", create_referral, name="pinax_referrals_create_referral"),
-    url(r"^(?P<code>\w+)/$", process_referral, name="pinax_referrals_process_referral")
+    url(r"^(?P<code>[-\w]+)/?$", process_referral, name="pinax_referrals_process_referral")
 ]
